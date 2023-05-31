e91552ce11eaed95185ad5f29cbdff0f74e770a3
Hebaabbas <heba1996abbas@gmail.com>
2023-05-31 10:32:31 +0000

    add a function to check the values and if theres double bookings

--- assets/assets.js
+++ assets/assets.js
@@ -7,3 +7,18 @@ let bookedTables = 0;
 
 // Array to store booked dates and times
 const bookedSlots = [];
+
+document.getElementById("bookingForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    //to check the values
+    const bookingDate = document.getElementById("bookingDate").value;
+    const bookingTime = document.getElementById("bookingTime").value;
+    const numGuests = parseInt(document.getElementById("numGuests").value);
+  
+    //to check for double bookings
+    if (isDoubleBooking(bookingDate, bookingTime)) {
+      alert("Please select a different time, the selected date and time are not available.");
+      return;
+    }
+    
