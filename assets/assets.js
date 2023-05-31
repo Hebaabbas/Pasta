f0037e46ddec2f9d37b429f0b5fc51e7910feb50
Hebaabbas <heba1996abbas@gmail.com>
2023-05-31 10:29:30 +0000

    add initiate commits to start off the java script with defining tables data

---
+++ assets/assets.js
@@ -0,0 +1,9 @@
+// Define the maximum number of tables is 10 and chairs per table is 4
+const maxTables = 10;
+const chairsPerTable = 4;
+
+// Track the number of booked tables
+let bookedTables = 0;
+
+// Array to store booked dates and times
+const bookedSlots = [];
